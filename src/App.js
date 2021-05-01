56e4f293b76eedce162afd0089f9daf567275099
Simon Körner <simonkoerner@gmx.de>
2021-05-02 01:13:28 +0200

    Initial commit with Tailwind and react-router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import About from './Views/About'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h3 className='text-2xl'>Hello React World!</h3>
+      </div>
+
+      <Switch>
+        <Route path='/about'>
+          <About></About>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Views/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <h1>Whataboutism</h1>
+        </div>
+    )
+}
